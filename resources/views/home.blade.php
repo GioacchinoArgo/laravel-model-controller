94fb6c85a9b4d84266795ad1b59ea2a84f699fbb
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-03-11 18:39:39 +0100

    added card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,5 +9,20 @@
     @vite('resources/js/app.js')
 </head>
 <body>
+    <div class="container pt-2">
+        <div class="row row-cols-2 align-items-end">
+            @foreach ($movies as $movie)
+            <div class="col d-flex justify-content-center align-items-center pt-4">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h5 class="card-title mt-2">{{ $movie->title }}</h5>
+                        <h6 class="card-subtitle text-muted mt-2">{{ $movie->original_title }}</h6>
+                        <p class="card-nationality mt-2">{{ $movie->nationality }}</p>
+                    </div>
+                </div>      
+            </div>
+            @endforeach
+        </div>
+    </div>
 </body>
 </html>
